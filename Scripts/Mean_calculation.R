4124a1f1850c3fad87d98f2fa761ee8b77431588
Phil Martin <philip.martin.1981@gmail.com>
2015-07-27 12:01:07 +0100

    added part script to make comparisons for flux tower

--- Scripts/Mean_calculation.R
+++ Scripts/Mean_calculation.R
@@ -13,3 +13,16 @@ ggplot(leafb_mean,aes(x=Month,y=mean_leaf))+geom_point()+geom_line()
 Nit_precip<-data.frame(Precip=c(800,900,1000),Nit_dep=c(1,1.35,1.7))
 
 lm(Nit_dep~Precip,data=Nit_precip)
+
+Landis_comparison<-read.csv("Landis_v1/Century-succession-monthly-log_comp.csv")
+View(Landis_comparison)
+Landis_comparison$N_month<-NA
+Landis_comparison$N_month[1]<-1
+for (i in 2:nrow(Landis_comparison)){
+  Landis_comparison$N_month[i]<-Landis_comparison$N_month[i-1]+1
+}
+
+
+Comp_comp1<-ggplot(Landis_comparison,aes(x=N_month,y=avgNEE,group=Time))+geom_point(colour="red")+geom_line(alpha=0.2,colour="red")
+Comp_comp2<-Comp_comp1+geom_point(data=Landis_comparison,aes(y=NEE_measured),colour="blue")+geom_line(alpha=0.2,colour="blue",aes(y=NEE_measured))
+Comp_comp2+ylab("NEE")+xlab("Number of months after first measurement")
