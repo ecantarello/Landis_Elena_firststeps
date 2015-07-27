b8ae02d63bd400096c06d72c3270d875daf4fb61
Phil Martin <philip.martin.1981@gmail.com>
2015-07-27 11:34:26 +0100

    added regression line

--- Scripts/Mean_calculation.R
+++ Scripts/Mean_calculation.R
@@ -7,4 +7,9 @@ str(Landis_calibrate)
 leafb<-ddply(Landis_calibrate,.(Year,Month),summarise,mean_leaf=mean(CohortLeafB))
 leafb_mean<-ddply(Landis_calibrate,.(Month),summarise,mean_leaf=mean(CohortLeafB))
 
-ggplot(leafb_mean,aes(x=Month,y=mean_leaf))+geom_point()
+ggplot(leafb_mean,aes(x=Month,y=mean_leaf))+geom_point()+geom_line()
+
+
+Nit_precip<-data.frame(Precip=c(800,900,1000),Nit_dep=c(1,1.35,1.7))
+
+lm(Nit_dep~Precip,data=Nit_precip)
