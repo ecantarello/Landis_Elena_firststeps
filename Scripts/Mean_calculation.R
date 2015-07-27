04a8c7d6eb201d3c03322a8619f8d57ac3476d2b
Phil Martin <philip.martin.1981@gmail.com>
2015-07-27 11:19:11 +0100

    drew scatterplot

--- Scripts/Mean_calculation.R
+++ Scripts/Mean_calculation.R
@@ -1,7 +1,10 @@
 library(plyr)
+library(ggplot2)
 
 Landis_calibrate<-read.csv("Landis_v1/Century-calibrate-log.csv")
 str(Landis_calibrate)
 
-ddply(Landis_calibrate,.(Year,Month),summarise,mean_leaf=mean(CohortLeafB))
+leafb<-ddply(Landis_calibrate,.(Year,Month),summarise,mean_leaf=mean(CohortLeafB))
+leafb_mean<-ddply(Landis_calibrate,.(Month),summarise,mean_leaf=mean(CohortLeafB))
 
+ggplot(leafb_mean,aes(x=Month,y=mean_leaf))+geom_point()
